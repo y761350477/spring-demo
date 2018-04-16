1ce1b01db17414c475610c41eac7d5b17707a6e2
y761350477 <761350477@qq.com>
2018-04-16 09:53:24 +0800

    1. Spring属性占位符的使用;

---
+++ src/main/java/test1/BeanTest1.java
@@ -0,0 +1,14 @@
+package test1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanTest1 {
+
+    @Value("${disc.title}")
+    public String properties_test;
+
+
+
+}
---
+++ src/main/java/test1/Config.java
@@ -0,0 +1,19 @@
+package test1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@ComponentScan("test1")
+@Configuration
+// 声明属性源(classpath: 可以省略)
+//@PropertySource("classpath:data.properties")
+public class Config {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
